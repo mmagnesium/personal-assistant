16ec805ddf8fea17ba2b702f036bd4547a1982fa
Oleh Kuznetsov <oposteriori+code@protonmail.com>
2023-05-19 21:40:11 +0200

    Add simple resnonse reviewer, remove redundant code

--- constants.py
+++ constants.py
@@ -13,4 +13,26 @@ CHROMA_SETTINGS = Settings(
         anonymized_telemetry=False
 )
 
+retriever_tempate = '''### Instruction: Use the following pieces of context to answer the question at the end. 
+If you don't know the answer, just say that you don't know, don't try to make up an answer. Try to be as precise as possible. The response should be helpful and informative, but concise. 
+Try to formulate the qeustions to the context, that would help to find the answer on the question.
+Dont include original context text in your response, only use your own words. Give the final answer in a form of the context summary.
 
+{context_proc}
+
+Question: {question}
+
+### Assistant: Let's work this out in a step by step way to be sure we have the right answer.'''
+
+resolver_template = '''### Instruction: You are the reviewer tasked to evaluate the correctness of the provided response, based on the query and context.
+Make sure that the context and response baised on it (provided below) are corrent and relevant to the query.
+If the response is not based on the context, or the context is irrelevant, reject it and respond with "The response of the retrieval agent is incorrect".
+If the response and the context are correct, finilize it. Ensure that the final response is concise, helpful and factual.
+
+Question: {question}
+
+Context: {context}
+
+Response: {response}
+
+### Assistant: Let' work this out in a step-by-step manner, to ensure that we understant the reponse and user query correctly:'''
--- main.py
+++ main.py
@@ -3,29 +3,22 @@ import os
 
 from llama_cpp import Llama
 
-
-from langchain.llms import LlamaCpp
-from langchain import PromptTemplate, LLMChain
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import Chroma
 
-from langchain.agents import initialize_agent, AgentType
-
-# import streamlit as st
+from constants import retriever_tempate, resolver_template
 
 load_dotenv()
 
-PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY')
+PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY', './db')
 SOURCE_DIRECTORY = os.environ.get('SOURCE_DIRECTORY', './files')
 
 def main():
     model_path="./models/wizard-mega-13B.ggml.q4_0.bin"
-    max_tokens=512
-    temperature=0.2
-    context_size = 2048
+    max_tokens=1024
+    temperature_retrieval=0.2
+    temperature_reviewer=0.2
+    context_size = 4096
     
     print(f'Loading the {model_path.split("/")[-1]} model...')
     llm = Llama(model_path=model_path, 
@@ -33,21 +26,11 @@ def main():
                 n_gpu_layers=40, 
                 n_ctx=context_size,
             )
-    
-    # callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-    # llm = LlamaCpp(
-    #     model_path=model_path, 
-    #     callback_manager=callback_manager, 
-    #     verbose=True,
-    #     n_gpu_layers=40,
-    #     n_ctx=context_size,
-    #     max_tokens=max_tokens,
-    #     temperature=temperature
-    # )
+
     print(f'Done')
     
     print(f'Loading the embedding model...')
-    embedding_instruction = "Represent the question for retrieving supporting documents; Input: "
+    embedding_instruction = "Represent the question for retrieving supporting documents "
     instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl", 
                                                           model_kwargs={"device": "cuda"},
                                                           query_instruction=embedding_instruction)
@@ -74,44 +57,47 @@ def main():
                 context_proc += 'Information: ' + doc.page_content + '\n' 
             else:
                 context_included.append(False)
+                
 
-        query = f'''### Instruction: Use the following pieces of context to answer the question at the end. 
-        If you don't know the answer, just say that you don't know, don't try to make up an answer. Try to be as precise as possible. The response should be helpful and informative, but concise. 
-        Dont include original context text in your response, only use your own words. Give the final answer in a form of the context summary.
-
-        {context_proc}
-
-        Question: {question}
-
-        ### Assistant: Let's work this out in a step by step way to be sure we have the right answer.'''
-        
-        # prompt_template = PromptTemplate(template=template, input_variables=["context_proc", "question"])
-        # llm_chain = LLMChain(prompt=prompt_template, 
-        #                      llm=llm)
-        
-        # res = llm_chain.run(question=query, context_proc=context_proc)
-        
+        query = retriever_tempate.format(context_proc=context_proc, question=question)  
+        # Retirval
         output_stream = llm(query,
                             max_tokens=max_tokens,
-                            temperature=temperature,
+                            temperature=temperature_retrieval,
                             top_p=0.95,
                             stream=True)
-
+        
         print('-'*20)
+        print('Retrival agent response: \n\n')
 
         output_response = ""
         for output in output_stream:
             o_text = output['choices'][0]['text']
             output_response += o_text
             print(o_text, sep='', end='', flush=True)
+            
         
-        # Print the result
-        # print("\n\n> Question:")
-        # print(query)
-        # print("\n> Answer:")
-        # print(res.strip('\n'))
-        
+        llm.reset()
+    
         print('-'*20)
+        
+        print('Reviewer agent response: \n\n')
+        
+        query_reviwer = resolver_template.format(response=output_response, 
+                                                 question=question,
+                                                 context=context_proc)
+        
+        output_stream = llm(query_reviwer,
+                            max_tokens=max_tokens,
+                            temperature=temperature_reviewer,
+                            top_p=0.95,
+                            stream=True)
+        
+        output_response = ""
+        for output in output_stream:
+            o_text = output['choices'][0]['text']
+            output_response += o_text
+            print(o_text, sep='', end='', flush=True)
         
         print(f'Sources: \n\n')
         for i, doc in enumerate(context_docs):
